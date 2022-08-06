0437265bb16ddc4e850be3c497f50ea9fa6378b0
Shahzad <mech1090@gmail.com>
2022-08-06 11:43:22 -0600

    db connection established

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const cors = require('cors')
+const config = require('config')
+const mongoose = require('mongoose')
+require('./db')
+
+const app = express()
+
+app.use(express())
+app.use(cors())
+app.use(express.json())
+app.use(express.static("public"))
+app.use(express.urlencoded({extended:true}))
+
+app.get('/',(req,res)=>{
+    res.send('TESTING')
+})
+
+app.get('*',(req,res)=>{
+    res.send('Bad_request')
+})
+
+mongoose.connection.once('open',()=>{
+    app.listen(config.get('port'),()=>{
+        console.log(`Server is running on port ${config.get('port')}`)
+    })
+    console.log('DB CONNECTED')
+    
+})
+
